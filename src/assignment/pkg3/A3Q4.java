a4b3e48afa10500f6c6a0850a915ac44745211d0
messr2578 <messr2578@HRH-IC0027340.wrdsb.ca>
2017-10-06 10:03:07 -0400

    getting it to be able to be used elsewhere

---
+++ src/assignment/pkg3/A3Q4.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package assignment.pkg3;
+
+/**
+ *
+ * @author messr2578
+ */
+public class A3Q4 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public int countingSort(int[] array){
+        
+    }
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
