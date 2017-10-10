24465d34ca5b3c99c4a819bc4bc63945f25444cc
messr2578 <messr2578@HRH-IC0027340.wrdsb.ca>
2017-10-10 09:57:49 -0400

    getting it to be able to be used elsewhere

--- src/assignment/pkg3/A3Q4.java
+++ src/assignment/pkg3/A3Q4.java
@@ -20,15 +20,26 @@ public class A3Q4 {
         for(int i = 0; i <array.length;i++){
                spot[array[i]] = spot[array[i]]+1;
         }
-        
-        
-        System.out.println(spot);
+        for(int j = 0; j <array.length;j++){
+            
+        }
+        for(int f = 0; f <array.length;f++){
+        System.out.println(array);
+        }
         return 0;
     }
     public static void main(String[] args) {
         A3Q4 test = new A3Q4();
         
         int[] numb = new int[101];
-        test.countingSort(numb);
+        numb[0] = 15;
+        for(int j = 1; j <numb.length;j++){
+            if(numb[j-1]%10 <=2){
+            numb[j] = numb[j-1]/2+1;
+           }else{
+             numb[j] = numb[j-1]*2+1;
+            }
+        }
+        int idc =test.countingSort(numb);
     }
 }
